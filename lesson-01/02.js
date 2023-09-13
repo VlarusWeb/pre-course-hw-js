32564a457f327ad8b794206273ed964af3d33dd6
vlarusweb <vlarusweb@gmail.com>
2023-09-14 02:02:36 +0300

    feat: add solution for 'lesson-01/02.js'

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,1 +1,4 @@
 // code
+const checkRandNumForOdd = (num = (Math.floor(Math.random() * 100))) => num % 2 === 0 ? `${num}: Это четное число` : `${num}: Это нечетное число`;
+
+console.log(checkRandNumForOdd());
