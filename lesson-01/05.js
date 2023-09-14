2cae15d0e7505f226416abe2c1873fa053178ec2
vlarusweb <vlarusweb@gmail.com>
2023-09-14 03:14:25 +0300

    feat: add lesson-01/06.js

---
+++ lesson-01/05.js
@@ -0,0 +1,16 @@
+let rule = "Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом.";
+
+switch (true) {
+  case rule.length < 25:
+    console.log("Все таки нет правил без исключения");
+    break;
+  case rule.length === 25:
+    console.log("50 на 50");
+    break;
+  case rule.length > 25:
+    console.log("У меня 100 пудов все получится");
+    break;
+  default:
+    console.log("The variable 'rule' is undefined and doesn't have a method 'length'!");
+    break;
+}
